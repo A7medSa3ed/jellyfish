a5dca0caff913f01245c2c5646d3c05b4be7e849
Mathspy <mathspy257@gmail.com>
2019-06-17 18:33:38 +0200

    replacing each individual page with pages.stories

---
+++ stories/pages.stories.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { linkTo } from "@storybook/addon-links";
+
+import WelcomePage from "../src/pages/WelcomePage";
+import ModelAnswerPage from "../src/pages/ModelAnswerPage";
+
+storiesOf("pages", module)
+  .add("to WelcomePage", () => {
+    return <WelcomePage />;
+  })
+  .add("to ModelAnswerPage", () => {
+    return (
+      <ModelAnswerPage
+        answers={[
+          { questionNumber: 0, Answer: "B" },
+          { questionNumber: 1, Answer: "B" },
+          { questionNumber: 2, Answer: "B" },
+          { questionNumber: 3, Answer: "B" },
+          { questionNumber: 4, Answer: "B" },
+          { questionNumber: 5, Answer: "B" }
+        ]}
+      />
+    );
+  });
