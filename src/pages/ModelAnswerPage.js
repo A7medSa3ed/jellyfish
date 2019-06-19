c52f6919d31945d5e80e8aaf28bcb8ea532349f9
Mathspy <mathspy257@gmail.com>
2019-06-19 12:36:29 +0200

    Model Answers page semi-finished

--- src/pages/ModelAnswerPage.js
+++ src/pages/ModelAnswerPage.js
@@ -2,29 +2,45 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 
 import Table from "../components/Table";
+import { getBase64 } from "../core";
 
-export default function ModelAnswerPage({ answers }) {
+export default function ModelAnswerPage({ success, answers, model }) {
   const [MCQGrades, setMCQGrades] = React.useState(
-    Array.from({ length: answers.mcq.length }).fill(1)
+    answers.mcq.map(({ answers }) => (answers.some(ans => ans) ? 1 : 0))
   );
 
   const [trueOrFalseGrades, setTrueOrFalseGrades] = React.useState(
-    Array.from({ length: answers.trueOrFalse.length }).fill(1)
+    answers.true_false.map(({ answers }) => (answers.some(ans => ans) ? 1 : 0))
   );
+
+  const [renderedModel, setRenderedModel] = React.useState(null);
+
+  React.useEffect(() => {
+    getBase64(model)
+      .then(setRenderedModel)
+      .catch(console.error);
+  }, [model]);
+
+  const grades = { mcq: MCQGrades, true_false: trueOrFalseGrades };
 
   return (
     <div style={{ display: "flex" }}>
       <div>
         <Table
           answers={answers}
-          grades={{ mcq: MCQGrades, trueOrFalse: trueOrFalseGrades }}
-          setGrades={{ mcq: setMCQGrades, trueOrFalse: setTrueOrFalseGrades }}
+          grades={grades}
+          setGrades={{ mcq: setMCQGrades, true_false: setTrueOrFalseGrades }}
         />
-        <Button variant="contained" color="primary" style={{ margin: 20 }}>
+        <Button
+          variant="contained"
+          color="primary"
+          style={{ margin: 20 }}
+          onClick={() => success(grades)}
+        >
           Submit
         </Button>
       </div>
-      <img src="https://via.placeholder.com/700" style={{}} />
+      {renderedModel && <img src={renderedModel} alt="Model Answers" />}
     </div>
   );
 }
